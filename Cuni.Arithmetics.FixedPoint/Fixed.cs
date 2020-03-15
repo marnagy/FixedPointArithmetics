ec47471f5726cc959023a35de1ceff02a083a3d1
Marek Nagy <62247071@cuni.cz>
2020-03-15 17:03:41 +0100

    Add project files.

---
+++ Cuni.Arithmetics.FixedPoint/Fixed.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cuni.Arithmetics.FixedPoint
+{
+    public struct Fixed<T>
+    {
+
+    }
+}
---
+++ FixedPointLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace FixedPointLib
+{
+    public class Class1
+    {
+    }
+}
