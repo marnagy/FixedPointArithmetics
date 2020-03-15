3b3c11111db4d07a917ee06aa27be842e940e9e1
Marek Nagy <62247071@cuni.cz>
2020-03-15 20:43:05 +0100

    Implementation of Fixed<T> and Q classes

--- Cuni.Arithmetics.FixedPoint/Fixed.cs
+++ Cuni.Arithmetics.FixedPoint/Fixed.cs
@@ -4,8 +4,33 @@ using System.Text;
 
 namespace Cuni.Arithmetics.FixedPoint
 {
-    public struct Fixed<T>
+    public struct Fixed<T> where T: Q, new()
     {
+        public readonly int Value;
+        public static int LowerBits;
+        static Fixed()
+        {
+            LowerBits = new T().GetLower();
+        }
+        public Fixed(int value) : this()
+        {
+            Console.WriteLine("Int32 .ctor");
+            this.Value = value << LowerBits;
+        }
+        private Fixed(long value) : this()
+        {
+            Console.WriteLine("Int64 .cctor");
+            this.Value = (int)value;
+        }
 
+
+        public Fixed<T> Add(Fixed<T> f) =>
+            new Fixed<T>((long)this.Value + f.Value);
+        public Fixed<T> Subtract(Fixed<T> f) =>
+            new Fixed<T>((long)this.Value - f.Value);
+        public Fixed<T> Multiply(Fixed<T> f) =>
+            new Fixed<T>(((long)this.Value * f.Value) << (LowerBits*2));
+        public Fixed<T> Divide(Fixed<T> f) =>
+            new Fixed<T>(((long)this.Value / f.Value) << LowerBits);
     }
 }
---
+++ Cuni.Arithmetics.FixedPoint/Q.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cuni.Arithmetics.FixedPoint
+{
+    public abstract class Q
+    {
+        public abstract int GetLower();
+    }
+    public class Q24_8 : Q
+    {
+        public override int GetLower() => 8;
+    }
+    public class Q16_16 : Q
+    {
+        public override int GetLower() => 16;
+    }
+    public class Q8_24 : Q
+    {
+        public override int GetLower() => 24;
+    }
+}
