71e8870c9cca505fd42a5019ffe996bf5ab8d13d
Marek Nagy <62247071@cuni.cz>
2020-04-17 16:55:42 +0200

    Imported libraries to benchmark

---
+++ BenchmarkProj/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace BenchmarkProj
+{
+	class Program
+	{
+		[Benchmark]
+		public static void Add()
+		{
+
+		}
+		[Benchmark]
+		public static void Subtract()
+		{
+
+		}
+		[Benchmark]
+		public static void Multiply()
+		{
+
+		}
+		[Benchmark]
+		public static void Divide()
+		{
+
+		}
+		static void Main(string[] args)
+		{
+			var summary = BenchmarkRunner.Run<Program>();
+		}
+	}
+}
