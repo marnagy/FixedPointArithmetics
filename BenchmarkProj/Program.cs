aec8c2b04d22c253ffb9acbe4554774be1100b8a
Marek Nagy <62247071@cuni.cz>
2020-04-17 17:20:52 +0200

    [WIP] Added methods WithoutLong and proper implementation

--- BenchmarkProj/Program.cs
+++ BenchmarkProj/Program.cs
@@ -1,30 +1,58 @@
 ﻿using System;
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
+using Cuni.Arithmetics.FixedPoint;
 
 namespace BenchmarkProj
 {
-	class Program
+	public class Program
 	{
-		[Benchmark]
-		public static void Add()
+		public static Fixed<Q24_8> a;
+		public static Fixed<Q24_8> b;
+		static Program()
 		{
-
+			a = new Fixed<Q24_8>(10);
+			b = new Fixed<Q24_8>(15);
 		}
 		[Benchmark]
-		public static void Subtract()
+		public void AddWithLong()
 		{
-
+			var res = a.Add(b);
 		}
 		[Benchmark]
-		public static void Multiply()
+		public void AddWithoutLong()
 		{
-
+			
 		}
 		[Benchmark]
-		public static void Divide()
+		public void SubtractWithLong()
 		{
-
+			var res = a.Subtract(b);
+		}
+		[Benchmark]
+		public void SubtractWithoutLong()
+		{
+			
+		}
+		[Benchmark]
+		public void MultiplyWithLong()
+		{
+			var res = a.Multiply(b);
+		}
+		[Benchmark]
+		public void MultiplyWithoutLong()
+		{
+			
+		}
+		[Benchmark]
+		public void DivideWithLong()
+		{
+			var res = a.Divide(b);
+		}
+		[Benchmark]
+		public void DivideWithoutLong()
+		{
+			
 		}
 		static void Main(string[] args)
 		{
